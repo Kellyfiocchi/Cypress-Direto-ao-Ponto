3fd127cd39df0970e405eb32fc5553ad7abf9383
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2023-08-29 20:43:24 -0300

    initial commit

---
+++ cypress/e2e/finances.cy.js
@@ -0,0 +1,11 @@
+describe("Transações", () => {
+  it("Cadastrar uma entrada", () => {
+    cy.visit("https://devfinance-agilizei.netlify.app/#");
+
+    cy.contains("Nova Transação").click();
+    cy.get("#description").type("Freelance");
+    cy.get("#amount").type("1000");
+    cy.get("#date").type("2023-08-29");
+    cy.get("button").contains("Salvar").click();
+  });
+});
