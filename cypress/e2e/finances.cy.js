e4e274de9b21edf266d0af52ca76e4389c208a98
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2023-08-29 21:04:50 -0300

    new update

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -8,4 +8,14 @@ describe("Transações", () => {
     cy.get("#date").type("2023-08-29");
     cy.get("button").contains("Salvar").click();
   });
+
+  it("Cadastrar uma saída", () => {
+    cy.visit("https://devfinance-agilizei.netlify.app/#");
+
+    cy.contains("Nova Transação").click();
+    cy.get("#description").type("Aluguel");
+    cy.get("#amount").type("-500");
+    cy.get("#date").type("2023-08-29");
+    cy.get("button").contains("Salvar").click();
+  });
 });
